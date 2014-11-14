f35460ab8097008fd50da091c48c2e085f40c9b6
Victor Canal <victor.canal@criteriait.com>
2014-11-14 12:10:06 -0500

    Más Cambios

--- UTPPrototipo/Controllers/EmpresaController.cs
+++ UTPPrototipo/Controllers/EmpresaController.cs
@@ -22,5 +22,9 @@ namespace UTPPrototipo.Controllers
         {
             return View();
         }
+        public ActionResult Postulante()
+        {
+            return View();
+        }
 	}
 }
