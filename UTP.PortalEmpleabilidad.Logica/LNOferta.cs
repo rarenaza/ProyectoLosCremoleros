6c09b178e57a791c9fdf529c32990405422fa6e0
spaiter01 <spaiter_01@hotmail.com>
2014-11-18 15:59:20 -0500

    18Nov3:59

--- UTP.PortalEmpleabilidad.Logica/LNOferta.cs
+++ UTP.PortalEmpleabilidad.Logica/LNOferta.cs
@@ -7,15 +7,46 @@ using System.Threading.Tasks;
 using UTP.PortalEmpleabilidad.Datos;
 using UTP.PortalEmpleabilidad.Modelo;
 
+
 namespace UTP.PortalEmpleabilidad.Logica
 {
     public class LNOferta
     {
-        ADOferta adOferta = new ADOferta();
 
-        public DataTable Obtener()
+        ADOferta adOferta = new ADOferta();
+        
+        public List<Oferta> Oferta_Mostrar()
         {
-            return adOferta.Obtener();
+            List<Oferta> listaOferta = new List<Oferta>();
+
+            DataTable dtResultado = adOferta.Obtener();
+
+
+            for (int i = 0; i <= dtResultado.Rows.Count - 1; i++)
+            {
+                Oferta oferta = new Oferta();
+                   
+                
+
+                oferta.FechaPublicacion = Convert.ToDateTime(dtResultado.Rows[i]["FechaPublicacion"]);
+              
+                oferta.CargoOfrecido = dtResultado.Rows[i]["CargoOfrecido"].ToString();
+                oferta.Horario = Convert.ToDateTime(dtResultado.Rows[i]["Horario"]);
+            
+                oferta.RemuneracionOfrecida = Convert.ToDecimal(dtResultado.Rows[i]["RangoRemuneracionDesde"]);
+     
+                oferta.EstadoOferta = dtResultado.Rows[i]["EstadoOferta"].ToString();
+
+                listaOferta.Add(oferta);
+            }
+            return listaOferta;
+
+            
+
+
+
         }
+
     }
+
 }
--- UTP.PortalEmpleabilidad.Modelo/Vistas/Ofertas/VistaOfertaAlumno.cs
+++ UTP.PortalEmpleabilidad.Modelo/Vistas/Ofertas/VistaOfertaAlumno.cs
@@ -4,40 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UTP.PortalEmpleabilidad.Modelo
+namespace UTP.PortalEmpleabilidad.Modelo.Vistas.Ofertas
 {
-   public  class Oferta
+    public class VistaOfertaAlumno
     {
+
+        public DateTime FechaPublicacion { get; set; }
+
+        public string  Empresa { get; set; }
+
+
+        public string TipoTrabajo { get; set; }
 
         public int IdOferta { get; set; }
         public int IdEmpresa { get; set; }
         public string UsuarioPropietarioEmpresa { get; set; }
-        public string  EstadoOferta { get; set; }
-        public DateTime FechaPublicacion { get; set; }
+        public string EstadoOferta { get; set; }
+        
         public DateTime FechaFinRecepcionCV { get; set; }
         public DateTime FechaFinProceso { get; set; }
         public int IdEmpresaLocacion { get; set; }
-        public string  DescripcionOferta { get; set; }
+        public string DescripcionOferta { get; set; }
         public string TipoTrabajo { get; set; }
         public string TipoContrato { get; set; }
         public int DuracionContrato { get; set; }
         public string TipoCargo { get; set; }
-        public string CargoOfrecido { get; set; }
   
+
         public decimal RemuneracionOfrecida { get; set; }
-       
+
 
         public DateTime FechaInicioLabores { get; set; }
- 
+
         public DateTime Horario { get; set; }
         public string AreaEmpresa { get; set; }
         public int NumeroVacantes { get; set; }
         public int RequiereExperienciaLaboral { get; set; }
         public string GradoMinimoEstudio { get; set; }
         public string CreadoPor { get; set; }
-       public DateTime  FechaCreacion{ get; set; }
+        public DateTime FechaCreacion { get; set; }
         public string ModificadoPor { get; set; }
-       public DateTime  FechaModificacion{ get; set; }
+        public DateTime FechaModificacion { get; set; }
+
+
+
+
 
     }
 }
