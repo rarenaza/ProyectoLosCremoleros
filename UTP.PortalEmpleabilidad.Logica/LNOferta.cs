067b5a66790238359fb5f7d95242b1147560fcce
spaiter01 <spaiter_01@hotmail.com>
2014-11-18 16:52:50 -0500

    18Nov4:52

--- UTP.PortalEmpleabilidad.Logica/LNOferta.cs
+++ UTP.PortalEmpleabilidad.Logica/LNOferta.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UTP.PortalEmpleabilidad.Datos;
 using UTP.PortalEmpleabilidad.Modelo;
-
+using UTP.PortalEmpleabilidad.Modelo.Vistas.Ofertas;
 
 namespace UTP.PortalEmpleabilidad.Logica
 {
@@ -15,37 +15,72 @@ namespace UTP.PortalEmpleabilidad.Logica
 
         ADOferta adOferta = new ADOferta();
         
-        public List<Oferta> Oferta_Mostrar()
+        /// <summary>
+        /// Obtiene las Ofertas Para la Pantalla Oferta
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        //public List<Oferta> Oferta_Mostrar()
+        //{
+        //    List<Oferta> listaOferta = new List<Oferta>();
+
+        //    DataTable dtResultado = adOferta.Obtener();
+
+
+        //    for (int i = 0; i <= dtResultado.Rows.Count - 1; i++)
+        //    {
+        //        Oferta oferta = new Oferta();
+
+
+
+        //        oferta.FechaPublicacion = Convert.ToDateTime(dtResultado.Rows[i]["FechaPublicacion"]);
+
+        //        oferta.CargoOfrecido = dtResultado.Rows[i]["CargoOfrecido"].ToString();
+        //        oferta.Horario = Convert.ToDateTime(dtResultado.Rows[i]["Horario"]);
+
+        //        oferta.RemuneracionOfrecida = Convert.ToDecimal(dtResultado.Rows[i]["RangoRemuneracionDesde"]);
+
+        //        oferta.EstadoOferta = dtResultado.Rows[i]["EstadoOferta"].ToString();
+
+        //        listaOferta.Add(oferta);
+        //    }
+        //    return listaOferta;
+
+            
+
+        //}
+
+        public List<VistaOfertaAlumno> Oferta_Mostrar()
         {
-            List<Oferta> listaOferta = new List<Oferta>();
+            List<VistaOfertaAlumno> listaOferta = new List<VistaOfertaAlumno>();
 
             DataTable dtResultado = adOferta.Obtener();
 
 
             for (int i = 0; i <= dtResultado.Rows.Count - 1; i++)
             {
-                Oferta oferta = new Oferta();
-                   
-                
+                VistaOfertaAlumno oferta = new VistaOfertaAlumno();
+
+
 
                 oferta.FechaPublicacion = Convert.ToDateTime(dtResultado.Rows[i]["FechaPublicacion"]);
-              
+                oferta.Empresa = dtResultado.Rows[i]["Empresa"].ToString();
+
                 oferta.CargoOfrecido = dtResultado.Rows[i]["CargoOfrecido"].ToString();
                 oferta.Horario = Convert.ToDateTime(dtResultado.Rows[i]["Horario"]);
-            
+
                 oferta.RemuneracionOfrecida = Convert.ToDecimal(dtResultado.Rows[i]["RangoRemuneracionDesde"]);
-     
+
                 oferta.EstadoOferta = dtResultado.Rows[i]["EstadoOferta"].ToString();
 
                 listaOferta.Add(oferta);
             }
             return listaOferta;
 
-            
-
 
 
         }
+                     
 
     }
 
--- UTPPrototipo/Controllers/OfertaController.cs
+++ UTPPrototipo/Controllers/OfertaController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using UTP.PortalEmpleabilidad.Logica;
 using UTP.PortalEmpleabilidad.Modelo;
+using UTP.PortalEmpleabilidad.Modelo.Vistas.Ofertas;
 
 namespace UTPPrototipo.Controllers
 {
@@ -14,16 +15,29 @@ namespace UTPPrototipo.Controllers
         // GET: Oferta
         LNOferta Ln = new LNOferta();
 
+        //public ActionResult Oferta_Mostrar()
+        //{
+        //    List<Oferta> ListaOferta = new List<Oferta>();
+
+        //    ListaOferta = Ln.Oferta_Mostrar();
+
+        //    return View(ListaOferta);
+           
+        //}
+
         public ActionResult Oferta_Mostrar()
         {
-            List<Oferta> ListaOferta = new List<Oferta>();
+    
 
-            ListaOferta = Ln.Oferta_Mostrar();
+            List<VistaOfertaAlumno> listaOferta = new List<VistaOfertaAlumno>();
 
-            return View(ListaOferta);
-           
+            listaOferta = Ln.Oferta_Mostrar();
+
+            return View(listaOferta);
+
         }
       
+       
 
     }
 }
