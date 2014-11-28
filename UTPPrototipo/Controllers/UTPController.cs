870ec643b72573e571c6ea603455aaadd37052b9
Victor Canal <victor.canal@criteriait.com>
2014-11-28 18:51:27 -0500

    Nuevos Cambiazos

---
+++ UTPPrototipo/Controllers/UTPController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UTPPrototipo.Controllers
+{
+    public class UTPController : Controller
+    {
+        // GET: UTP
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Registros()
+        {
+            return View();
+        }
+        public ActionResult Portal()
+        {
+            return View();
+        }
+    }
+}
