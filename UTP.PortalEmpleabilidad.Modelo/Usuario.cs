be1fb4dd855da15b3e198bec4639c497427eedb5
criteriait <aldo.chocos@criteriait.com>
2014-12-15 09:49:08 -0500

    Registro de empresa 15DIC 9:48

--- UTP.PortalEmpleabilidad.Modelo/Usuario.cs
+++ UTP.PortalEmpleabilidad.Modelo/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,14 @@ namespace UTP.PortalEmpleabilidad.Modelo
     public class Usuario
     {
         public int id { get; set; }
+        
+        [Required(ErrorMessage=Constantes.MSJ_CAMPO_OBLIGATORIO)]
         public string NombreUsuario { get; set; }
         public ListaValor TipoUsuario { get; set; }
         public ListaValor EstadoUsuario { get; set; }
         public ListaValor Rol { get; set; }        
+        
+        [Required(ErrorMessage=Constantes.MSJ_CAMPO_OBLIGATORIO)]
         public string Contrasena { get; set; }
 
         public Usuario()
