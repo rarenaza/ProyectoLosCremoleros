4a909d30fff534dc36fc03493cc99958757bbd35
Williams Morales Caballero <williams_morales85@yahoo.es>
2014-12-19 14:25:30 -0500

    19 DIC 2014
    NUEVOS CAMBIOS

--- UTPPrototipo/Common/Util.cs
+++ UTPPrototipo/Common/Util.cs
@@ -25,7 +25,7 @@ namespace UTPPrototipo.Common
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            if (filterContext.HttpContext.Session["TicketEmpresa"] == null && filterContext.HttpContext.Session["TicketUTP"] == null)
+            if (filterContext.HttpContext.Session["TicketEmpresa"] == null && filterContext.HttpContext.Session["TicketUTP"] == null && filterContext.HttpContext.Session["TicketAlumno"] == null)
             {
                 filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "Index" }));
                 filterContext.Result.ExecuteResult(filterContext.Controller.ControllerContext);
