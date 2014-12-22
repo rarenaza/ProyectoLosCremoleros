1223083f4c5b63a58dcecdfe2f0b8d892951121e
criteriait <aldo.chocos@criteriait.com>
2014-12-22 18:23:41 -0500

    UTP Usuario crear y editar 22DIC 6:23 p.m.

---
+++ UTP.PortalEmpleabilidad.Modelo/UTPUsuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTP.PortalEmpleabilidad.Modelo
+{
+    public class UTPUsuario : Auditoria
+    {
+        public int IdUTPUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string SexoDescripcion { get; set; }
+        public string SexoIdListaValor { get; set; }
+        public string Correo { get; set; }
+        public string TelefonoFijo { get; set; }
+        public string TelefonoAnexo { get; set; }
+        public string TelefonoCelular { get; set; }
+        public string EstadoUsuarioDescripcion { get; set; }
+        public string EstadoUsuarioIdListaValor { get; set; }
+        public string RolDescripcion { get; set; }
+        public string RolIdListaValor { get; set; }
+        public string TipoUsuarioIdListaValor { get; set; }
+
+    }
+}
